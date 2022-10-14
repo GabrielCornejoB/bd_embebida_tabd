737fa28af9e4de060f7b4ad33f913e414e6fba47
Gabriel <gabrielcorbot@gmail.com>
2022-10-14 10:07:49 -0500

    BD creada (solo las tablas)

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ import sqlite3 as sql
 
 eel.init(os.path.dirname(os.path.realpath(__file__)) + "\\web")
 
+@eel.expose
+def select(table_name):
+    conn = sql.connect("pescasDB.sqlite")
+    cursor = conn.cursor()
+
+    query = cursor.execute("SELECT * FROM " + table_name)
+
+    conn.close()
+
+
 eel.start("index.html")

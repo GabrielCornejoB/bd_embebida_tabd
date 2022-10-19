e28508690b96c4f687a8ee4da9c0517da53e39ee
Gabriel <gabrielcorbot@gmail.com>
2022-10-19 13:21:33 -0500

    Diseño tabla operación READ, tambien tamaño para cards

--- web/js/crud.js
+++ web/js/crud.js
@@ -7,16 +7,17 @@ function load_table (output) {
     string_table = "";
     if (table_name === "cuencas" || table_name === "metodos") {
         if (table_name === "cuencas") {
-            table_string = "<tr><th>Id cuenca</th><th>Nombre cuenca</th></tr>";
+            table_string = "<thead><tr><th>Id Cuenca</th><th>Cuenca Hidrográfica</th></tr></thead><tbody>";
         }
         else if (table_name === "metodos") {
-            table_string = "<tr><th>Id método</th><th>Método de pesca</th></tr>";
+            table_string = "<thead><tr><th>Id método</th><th>Método de pesca</th></tr></thead><tbody>";
         }
         parsed_output.forEach(row => table_string = table_string.concat("<tr><td>", row[0] , "</td><td>", row[1], "</td></tr>"));
     }
     else if (table_name === "pescas") {
-        table_string = "<tr><th>Id pesca</th><th>Id cuenca</th><th>Id método</th><th>Fecha</th><th>Peso total</th></tr>";
+        table_string = "<thead><tr><th>Id pesca</th><th>Id cuenca</th><th>Id método</th><th>Fecha</th><th>Peso total</th></tr></thead><tbody>";
         parsed_output.forEach(row => table_string = table_string.concat("<tr><td>", row[0], "</td><td>", row[1], "</td><td>", row[2], "</td><td>", row[3], "</td><td>", row[4], "</td></tr>"));
     }
+    table_string = table_string.concat("</tbody>");
     document.getElementById("data").innerHTML = table_string;
 }

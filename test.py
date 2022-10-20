c248c21682b6898bff46f9194b8561940ecd989e
Gabriel <gabrielcorbot@gmail.com>
2022-10-20 18:41:30 -0500

    CREATE con validación de llaves foraneas

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import json
 import sys
 
 valid_tables = ["cuencas", "metodos", "pescas"]
+db = "pescasDB.sqlite"
 
 def check_args(args):
     if (type(args) is list) and len(args) > 0:
@@ -12,7 +13,15 @@ def check_args(args):
     return False
 
 def check_fk(val, table):
-    print("uwu")
+    conn = sql.connect(db)
+    cursor = conn.cursor()
+    id_name = "id_" + table[:-1]
+    query = "SELECT * FROM " + table + " WHERE " + id_name + "=(?)"
+    rows = cursor.execute(query, [val]).fetchall()
+    if(len(rows) > 0):
+        return True
+    else:
+        return False
 
 def select (table_name):
     try:
@@ -20,7 +29,7 @@ def select (table_name):
         if not table_name in valid_tables:
             raise Exception("Invalid table name!")
 
-        conn = sql.connect("pescasDB.sqlite")
+        conn = sql.connect(db)
         cursor = conn.cursor()
         rows = cursor.execute("SELECT * FROM " + table_name).fetchall()
 
@@ -33,7 +42,6 @@ def select (table_name):
     else:
         print(json.dumps(rows, ensure_ascii=False).encode('utf-8').decode())
 
-
 def create(table_name, args):
     try:
         table_name = table_name.lower()
@@ -43,7 +51,7 @@ def create(table_name, args):
             raise Exception("Invalid table name!")
         if not check_args(args):
             raise Exception("Invalid arg(s)!")
-
+        
         if (table_name == "cuencas" or table_name == "metodos"):
             if len(args) != 1:
                 raise Exception("Table \"" + table_name + "\" needs exactly 1 argument!")
@@ -60,9 +68,13 @@ def create(table_name, args):
                     args[3] = float(args[3])
                 except:
                     raise Exception("Args have invalid types")
+                if not check_fk(args[0], "cuencas"):
+                    raise Exception("Arg #1 doesn't exist in cuencas")
+                if not check_fk(args[1], "metodos"):
+                    raise Exception("Arg #2 doesn't exist in metodos")
                 query_end = " (id_cuenca, id_metodo, fecha_pesca, peso_total_pesca) VALUES (?, ?, ?, ?)"
         
-        conn = sql.connect("pescasDB.sqlite")
+        conn = sql.connect(db)
         cursor = conn.cursor()
         query = "INSERT INTO " + table_name + query_end
         cursor.execute(query, args)
@@ -73,3 +85,4 @@ def create(table_name, args):
         print("ERROR:",e)
     else:
         print("Entry created succesfully!")
+

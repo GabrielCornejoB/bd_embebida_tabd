ceff552f348c9df967a9a36a2aa21ee62b40a8b4
Gabriel <gabrielcorbot@gmail.com>
2022-10-20 11:09:19 -0500

    Método CREATE en consola

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import sqlite3 as sql
 import json
 
-
 def select (table_name):
     if (table_name.isalpha()):
         conn = sql.connect("pescasDB.sqlite")
@@ -12,3 +11,37 @@ def select (table_name):
             return json.dumps(rows, ensure_ascii=False).encode('utf-8').decode()
     else:
         return "[err] Input was not alphabetic only"
+
+def create(table_name, args):
+    valid_tables = ["cuencas", "metodos", "pescas"]
+    if (table_name.isalpha() and (table_name in valid_tables)):
+        conn = sql.connect("pescasDB.sqlite")
+        cursor = conn.cursor()
+        query = "INSERT INTO"
+
+        if (table_name == valid_tables[0]):
+            query = query + " cuencas (cuenca)"
+        elif (table_name == valid_tables[1]):
+            query = query + " metodos (metodo)"
+        elif (table_name == valid_tables[2]):
+            print("no implementado para tabla pescas todavía")
+
+        if(len(args) == 1 and args[0] and (not args[0].isspace())):
+            query = query + " VALUES (?)"
+            cursor.execute(query, args)
+        elif(len(args) == 2):
+            print('no implementado para tabla pescas todavía')
+        else:
+            conn.close()
+            return "[err] Invalid arguments"
+
+        conn.commit()
+        conn.close()
+    else:
+        return "[err] Table name not valid"
+
+
+            
+
+print(create("metodos", ["Abrelatas"]))
+print(select("metodos"))
